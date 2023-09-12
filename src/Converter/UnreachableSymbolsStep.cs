05a6f0386a3d79754ccacec709d415a1628ad813
Suzanne van der Veen <k.vanderveen-4@student.utwente.nl>
2023-09-12 11:50:14 +0200

    Initial commit for unreachable symbols step

---
+++ src/Converter/UnreachableSymbolsStep.cs
@@ -0,0 +1,22 @@
+﻿using NoWa.Common;
+using NoWa.Common.Logging;
+
+namespace NoWa.Converter;
+
+/// <summary>
+/// A conversion step that eliminates any unreachable symbols.
+/// </summary>
+public sealed class UnreachableSymbolsStep : BaseConversionStep
+{
+    /// <inheritdoc/>
+    public UnreachableSymbolsStep(ILogger logger) : base(logger) { }
+
+    /// <summary>
+    /// Eliminates all unreachable ('useless') symbols from the given grammar.
+    /// </summary>
+    /// <inheritdoc/>
+    public override void Convert(Grammar grammar)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/Converter/UnreachableSymbolsStepTests.cs
@@ -0,0 +1,42 @@
+﻿using NoWa.Common;
+
+namespace NoWa.Converter.Tests;
+
+/// <summary>
+/// Contains unit tests for the <see cref="UnreachableSymbolsStep"/>.
+/// </summary>
+[TestClass]
+public class UnreachableSymbolsStepTests
+{
+    /// <summary>
+    /// Tests the <see cref="UnreachableSymbolsStep"/>.
+    /// </summary>
+    /// <remarks>
+    /// This unit test is based on the example given by Hopcroft, Motwani, and Ullman on page 262 of the 2013 edition
+    /// of "Introduction to Automata Theory, Languages, and Computation: Pearson New International Edition" 
+    /// </remarks>
+    [TestMethod]
+    public void TestUnreachableSymbols()
+    {
+        Grammar grammar = new();
+
+        Terminal a = new("a");
+        Terminal b = new("b");
+
+        Nonterminal B = grammar.AddNonterminal("B");
+
+        Rule S = grammar.AddRule("S");
+        Rule A = grammar.AddRule("A");
+
+        S.AddProduction(A.Nonterminal, B);
+        S.AddProduction(a);
+
+        A.AddProduction(b);
+
+        TestLogger logger = new();
+        UnreachableSymbolsStep step = new(logger);
+        step.Convert(grammar);
+
+        Assert.AreEqual("S = 'a'", grammar.ToString());
+    }
+}
