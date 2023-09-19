92d3bd22d33701cdbdcc621f3600bc50d26a6c2b
Suzanne van der Veen <k.vanderveen-4@student.utwente.nl>
2023-09-19 16:01:02 +0200

    Implemented separation of terminals in large bodies

--- src/Converter/NoWaConverter.cs
+++ src/Converter/NoWaConverter.cs
@@ -31,7 +31,8 @@ public class NoWaConverter
         {
             new EmptyStringStep(Logger),
             new UnitProductionsStep(Logger),
-            new UnreachableSymbolsStep(Logger)
+            new UnreachableSymbolsStep(Logger),
+            new SeparateTerminalsStep(Logger)
         };
     }
 
@@ -65,37 +66,6 @@ public class NoWaConverter
         }
 
         return result;
-    }
-
-    /// <summary>
-    /// Eliminates nonsolitary terminals.
-    /// </summary>
-    /// <param name="grammar">The grammar to convert.</param>
-    public static void SeparateTerminals(Grammar grammar)
-    {
-        Console.WriteLine("Separating terminals...");
-        for (int i = 0, count = grammar.RuleCount; i < count; i++)
-        {
-            Rule rule = grammar.GetRule(i);
-            foreach (var expr in rule.Productions.Where(expr => expr.Count > 1))
-            {
-                for (int j = 0; j < expr.Count; j++)
-                {
-                    if (expr[j] is Terminal terminal)
-                    {
-                        // A nonsolitary terminal is replaced with a nonterminal across the entire grammar.
-                        Nonterminal nonterminal = grammar.AddNonterminal($"TERM-{terminal.Value.Replace(" ", "-")}");
-                        grammar.ReplaceSymbol(terminal, nonterminal, false); // Keep the original because we will insert it again in the new rule.
-                        // A new rule is added for the new nonterminal that refers to the old terminal.
-                        Rule newRule = new(nonterminal);
-                        newRule.AddProduction(terminal);
-                        grammar.AddRule(newRule);
-                    }
-                }
-            }
-        }
-        Console.WriteLine(grammar.ToString());
-        Console.WriteLine();
     }
 
     /// <summary>
--- src/Converter/SeparateTerminalsStep.cs
+++ src/Converter/SeparateTerminalsStep.cs
@@ -17,6 +17,103 @@ public sealed class SeparateTerminalsStep : BaseConversionStep
     /// <inheritdoc/>
     public override void Convert(Grammar grammar)
     {
-        throw new NotImplementedException();
+        Logger.LogInfo("Separating terminals from large bodies...");
+        int initialRuleCount = grammar.RuleCount;
+
+        for (int i = 0; i < grammar.RuleCount; i++)
+        {
+            SeparateTerminalsInRule(grammar, grammar.GetRule(i));
+        }
+
+        Logger.LogInfo("Terminals separated.");
+        if (initialRuleCount !=  grammar.RuleCount)
+        {
+            Logger.LogInfo($"\tIntroduced {grammar.RuleCount - initialRuleCount} rules.");
+        }
+    }
+
+    /// <summary>
+    /// Separate terminals in a rule into their own rules.
+    /// </summary>
+    /// <param name="grammar">The grammar to separate terminals in.</param>
+    /// <param name="rule">The rule to separate terminals in.</param>
+    private void SeparateTerminalsInRule(Grammar grammar, Rule rule)
+    {
+        foreach (Expression production in rule.Productions)
+        {
+            if (production.Count == 1)
+            {
+                continue;
+            }
+            SeparateTerminalsInProduction(grammar, production);
+        }
+    }
+
+    /// <summary>
+    /// Separate terminals in a production into their own rules.
+    /// </summary>
+    /// <param name="grammar">The grammar to separate the terminals in.</param>
+    /// <param name="production">The production to separate the terminals in.</param>
+    private void SeparateTerminalsInProduction(Grammar grammar, Expression production)
+    {
+        for (int i = 0; i < production.Count; i++)
+        {
+            if (production[i] is Terminal terminal)
+            {
+                ReplaceTerminalInGrammar(grammar, terminal);
+            }
+        }
+    }
+    /// <summary>
+    /// Replace a terminal in bodies larger than 2 with a nonterminal that produces it.
+    /// </summary>
+    /// <param name="grammar">The grammar to replace the terminal in.</param>
+    /// <param name="terminal">The terminal to replace.</param>
+    private void ReplaceTerminalInGrammar(Grammar grammar, Terminal terminal)
+    {
+        Logger.LogDebug($"Replacing terminal: {terminal}");
+        Rule newRule = grammar.AddRule($"T-{terminal.Value}"); // This could fail if a rule with that name already existed, but that is pretty unlikely. If it does: too bad!
+        newRule.AddProduction(terminal);
+        Logger.LogDebug($"New nonterminal: {newRule.Nonterminal}");
+
+        for (int i = 0; i < grammar.RuleCount; ++i)
+        {
+            ReplaceTerminalInRule(grammar.GetRule(i), terminal, newRule.Nonterminal);
+        }
+    }
+
+    /// <summary>
+    /// Replace all occurrences of a terminal in a rule with a nonterminal.
+    /// </summary>
+    /// <param name="rule">The rule to replace the terminal in.</param>
+    /// <param name="terminal">The terminal to replace.</param>
+    /// <param name="nonterminal">The nonterminal to replace the terminal with.</param>
+    private void ReplaceTerminalInRule(Rule rule, Terminal terminal, Nonterminal nonterminal)
+    {
+        foreach (Expression production in rule.Productions)
+        {
+            if (production.Count <= 1)
+            {
+                continue;
+            }
+            ReplaceTerminalInProduction(production, terminal, nonterminal);
+        }
+    }
+
+    /// <summary>
+    /// Replace all occurrences of a terminal in a production with a nonterminal.
+    /// </summary>
+    /// <param name="production">The production to replace the terminal in.</param>
+    /// <param name="terminal">The terminal to replace.</param>
+    /// <param name="nonterminal">The nonterminal to replace the terminal with.</param>
+    private void ReplaceTerminalInProduction(Expression production, Terminal terminal, Nonterminal nonterminal)
+    {
+        for (int i = 0; i < production.Count; ++i)
+        {
+            if (terminal.Equals(production[i]))
+            {
+                production[i] = nonterminal;
+            }
+        }
     }
 }
