b0291c9c08b8c3460c0df6ee00f0b91e8108be99
Suzanne van der Veen <k.vanderveen-4@student.utwente.nl>
2023-09-26 08:36:07 +0200

    Class layout and unit test for splitting nonterminals

---
+++ src/Converter/SplitNonterminalsStep.cs
@@ -0,0 +1,22 @@
+﻿using NoWa.Common;
+using NoWa.Common.Logging;
+
+namespace NoWa.Converter;
+
+/// <summary>
+/// A conversion step that splits productions with more than two nonterminals into multiple substeps.
+/// </summary>
+public sealed class SplitNonterminalsStep : BaseConversionStep
+{
+    /// <inheritdoc/>
+    public SplitNonterminalsStep(ILogger logger) : base(logger) { }
+
+    /// <summary>
+    /// Splits all productions with more than two nonterminals into multiple substeps.
+    /// </summary>
+    /// <inheritdoc/>
+    public override void Convert(Grammar grammar)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ test/Converter/SplitNonterminalsStepTests.cs
@@ -0,0 +1,48 @@
+﻿using NoWa.Common;
+
+namespace NoWa.Converter.Tests;
+
+/// <summary>
+/// Contains unit tests for the <see cref="SplitNonterminalsStep"/>.
+/// </summary>
+[TestClass]
+public class SplitNonterminalsStepTests
+{
+    /// <summary>
+    /// Tests the splitting of a single rule with three nonterminals.
+    /// </summary>
+    [TestMethod]
+    public void TestSplitOne()
+    {
+        Grammar grammar = new();
+
+        Terminal a = grammar.AddTerminal("a");
+        Terminal b = grammar.AddTerminal("b");
+        Terminal c = grammar.AddTerminal("c");
+
+        Nonterminal A = grammar.AddNonterminal("A");
+        Nonterminal B = grammar.AddNonterminal("B");
+        Nonterminal C = grammar.AddNonterminal("C");
+
+        Rule S = grammar.AddRule("S");
+        S.AddProduction(A, B, C);
+
+        Rule RA = grammar.AddRule("A");
+        RA.AddProduction(a);
+
+        Rule RB = grammar.AddRule("B");
+        RB.AddProduction(b);
+
+        Rule RC = grammar.AddRule("C");
+        RC.AddProduction(c);
+
+        SplitNonterminalsStep step = new(new TestLogger());
+        step.Convert(grammar);
+
+        Assert.AreEqual($"S = A B-C ;{Environment.NewLine}" +
+            $"A = 'a' ;{Environment.NewLine}" +
+            $"B = 'b' ;{Environment.NewLine}" +
+            $"C = 'c' ;{Environment.NewLine}" +
+            $"B-C = B C ;", grammar.ToString());
+    }
+}
