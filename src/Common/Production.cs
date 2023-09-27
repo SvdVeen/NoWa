397b5794d826acd8402f8a299bfa759e100b0606
Suzanne van der Veen <k.vanderveen-4@student.utwente.nl>
2023-09-27 15:49:46 +0200

    Production Class + Unit Tests

---
+++ src/Common/Production.cs
@@ -0,0 +1,76 @@
+﻿namespace NoWa.Common;
+
+/// <summary>
+/// Represents a production rule in a grammar.
+/// </summary>
+public class Production
+{
+    /// <summary>
+    /// Gets the head of the production rule.
+    /// </summary>
+    public Nonterminal Head { get; }
+
+    /// <summary>
+    /// Gets the body of the production rule.
+    /// </summary>
+    public Expression Body { get; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Production"/> class.
+    /// </summary>
+    /// <param name="head">The head of the production rule.</param>
+    /// <param name="body">The body of the production rule.</param>
+    public Production(Nonterminal head, Expression body)
+    {
+        Head = head;
+        Body = body;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Production"/> class.
+    /// </summary>
+    /// <param name="head">The head of the production rule.</param>
+    /// <param name="body">The body of the production rule.</param>
+    public Production(Nonterminal head, IEnumerable<ISymbol> body) : this(head, new Expression(body)) { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Production"/> class.
+    /// </summary>
+    /// <param name="head">The head of the production rule.</param>
+    /// <param name="body">The body of the production rule.</param>
+    public Production(Nonterminal head, params ISymbol[] body) : this(head, new Expression(body)) { }
+
+    /// <inheritdoc/>
+    public override string ToString()
+    {
+        if (Body.Count == 0)
+        {
+            return $"Empty production {Head} ;";
+        }
+        return $"{Head} --> {Body} ;";
+    }
+
+    /// <inheritdoc/>
+    public override bool Equals(object? obj)
+    {
+        return obj is Production production &&
+               EqualityComparer<Nonterminal>.Default.Equals(Head, production.Head) &&
+               EqualityComparer<Expression>.Default.Equals(Body, production.Body);
+    }
+
+    /// <inheritdoc/>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Head, Body);
+    }
+
+    public static bool operator ==(Production? left, Production? right)
+    {
+        return EqualityComparer<Production>.Default.Equals(left, right);
+    }
+
+    public static bool operator !=(Production? left, Production? right)
+    {
+        return !(left == right);
+    }
+}
---
+++ test/Common/ProductionTests.cs
@@ -0,0 +1,118 @@
+﻿namespace NoWa.Common.Tests;
+
+/// <summary>
+/// Contains unit tests for the <see cref="Production"/> class.
+/// </summary>
+[TestClass]
+public class ProductionTests
+{
+    /// <summary>
+    /// Tests the <see cref="Production(Nonterminal, Expression)"/> constructor.
+    /// </summary>
+    [TestMethod]
+    public void TestProduction1()
+    {
+        Production production = new(Nonterminal.Get("A"), new Expression()
+        {
+            Nonterminal.Get("B"),
+            Terminal.Get("a")
+        });
+        Assert.AreSame(Nonterminal.Get("A"), production.Head);
+        Assert.AreEqual(2, production.Body.Count);
+        Assert.AreSame(Nonterminal.Get("B"), production.Body[0]);
+        Assert.AreSame(Terminal.Get("a"), production.Body[1]);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production(Nonterminal, IEnumerable{ISymbol})"/> constructor.
+    /// </summary>
+    [TestMethod]
+    public void TestProduction2()
+    {
+        Production production = new(Nonterminal.Get("A"), new List<ISymbol> {
+            Nonterminal.Get("B"),
+            Terminal.Get("a")
+        });
+        Assert.AreSame(Nonterminal.Get("A"), production.Head);
+        Assert.AreEqual(2, production.Body.Count);
+        Assert.AreSame(Nonterminal.Get("B"), production.Body[0]);
+        Assert.AreSame(Terminal.Get("a"), production.Body[1]);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production(Nonterminal, ISymbol[])"/> constructor.
+    /// </summary>
+    [TestMethod]
+    public void TestProduction3()
+    {
+        Production production = new(Nonterminal.Get("A"), Nonterminal.Get("B"), Terminal.Get("a"));
+        Assert.AreSame(Nonterminal.Get("A"), production.Head);
+        Assert.AreEqual(2, production.Body.Count);
+        Assert.AreSame(Nonterminal.Get("B"), production.Body[0]);
+        Assert.AreSame(Terminal.Get("a"), production.Body[1]);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.ToString"/> method for a non-empty production.
+    /// </summary>
+    [TestMethod]
+    public void TestToString()
+    {
+        Production production = new(Nonterminal.Get("A"), Terminal.Get("b"), Terminal.Get("c"));
+        Assert.AreEqual("A --> 'b' 'c' ;", production.ToString());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.ToString"/> method for an empty production.
+    /// </summary>
+    [TestMethod]
+    public void TestToStringEmpty()
+    {
+        Production production = new(Nonterminal.Get("A"));
+        Assert.AreEqual("Empty production A ;", production.ToString());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.Equals(object?)"/> method for two equal productions.
+    /// </summary>
+    [TestMethod]
+    public void TestEqualsEqual()
+    {
+        Production a = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Production b = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Assert.AreEqual(a, b);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.Equals(object?)"/> method for two non-equal productions.
+    /// </summary>
+    [TestMethod]
+    public void TestEqualsNotEqual()
+    {
+        Production a = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Production b = new Production(Nonterminal.Get("B"), Terminal.Get("b"));
+        Assert.AreNotEqual(a, b);
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.GetHashCode"/> method for two non-equal productions.
+    /// </summary>
+    [TestMethod]
+    public void TestGetHashCodeEqual()
+    {
+        Production a = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Production b = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Assert.AreEqual(a.GetHashCode(), b.GetHashCode());
+    }
+
+    /// <summary>
+    /// Tests the <see cref="Production.GetHashCode"/> method for two non-equal productions.
+    /// </summary>
+    [TestMethod]
+    public void TestGetHashCodeNotEqual()
+    {
+        Production a = new Production(Nonterminal.Get("A"), Terminal.Get("a"));
+        Production b = new Production(Nonterminal.Get("B"), Terminal.Get("b"));
+        Assert.AreNotEqual(a.GetHashCode(), b.GetHashCode());
+    }
+}
